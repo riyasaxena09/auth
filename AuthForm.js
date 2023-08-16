b9ce8e61da074765501fbba192b433577ead47f2
riyasaxena09 <135497287+riyasaxena09@users.noreply.github.com>
2023-08-16 13:59:38 +0530

    add AuthForm.js

--- AuthForm.js
+++ AuthForm.js
@@ -17,40 +17,46 @@ e.preventDefault();
 const emailAmount=emailInput.current.value;
 const passAmount=passwordInput.current.value;
 setloading(true);
+let url;
 if(isLogin){
-
+ url='https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCuh7-IOQf7EvfysZe7Kf7bewepEyNN1VE';
 }else{
- fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCuh7-IOQf7EvfysZe7Kf7bewepEyNN1VE',
- {
-  method:'POST',
-  body:JSON.stringify({
-    email:emailAmount,
-    password:passAmount,
-    returnSecureToken:true,
-  }),
+ url='https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCuh7-IOQf7EvfysZe7Kf7bewepEyNN1VE'
  
- headers:{
-'Content-Type':'application/json'
- }
 }
- ).then(res=>{
+fetch(url,
+  {
+   method:'POST',
+   body:JSON.stringify({
+     email:emailAmount,
+     password:passAmount,
+     returnSecureToken:true,
+   }),
+  
+  headers:{
+ 'Content-Type':'application/json'
+  },
+}).then((res)=>{
+  setloading(false);
   if(res.ok){
-
+return res.json();
   }else{
-    setloading(false);
-    res.json().then((data)=>{
-let errormsg='Authentication Failed';
-if(data && data.error && data.error.message){
-  errormsg=data.error.message;
-}
-alert(errormsg)
-    })
+return res.json().then((data)=>{
+  let errormsg='Authen failed';
+  throw new Error(errormsg);
+})
   }
- })
+}).then((data)=>{
+console.log(data);
+}).catch((err)=>{
+  alert(err.message);
+})
+ 
+ 
+  
     
-  }
+  
 }
-
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
